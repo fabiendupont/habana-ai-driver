1b097fd74f95cb43fb64f630fd479a91ead22a97
Maor Walter <mwalter@habana.ai>
2023-03-09 12:14:32 +0200

    query OCP version instead of iterating over version until not existing, guess kernel version from kernel-rt version because com.coreos.rpm.kernel label was removed from machine-os-content image at OCP 4.12
    Signed-off-by: Maor Walter <mwalter@habana.ai>

--- build-matrix.sh
+++ build-matrix.sh
@@ -11,35 +11,28 @@ HABANA_VERSIONS=("1.6.0-439")
 
 # Retrieve all the unique kernel versions
 KVERS=()
-for y in $(seq 11 11); do
-    for z in $(seq 0 99); do
+declare -A KVERS_OCPVERS_MAP
+for y in $(seq 12 12); do
+    # The curl command will reteive all builds for 4.y openshift releace (4.y.0 4.y.1 ...)
+    for z in $(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/ | grep -Eo "4\.${y}\.[0-9]+" | sort -uV); do
         for a in "x86_64"; do
             # Get the release image for the z-stream
-            echo -n "Get the release image for OCP 4.${y}.${z}-${a}."
-            IMG=$(oc adm release info --image-for=machine-os-content quay.io/openshift-release-dev/ocp-release:4.${y}.${z}-${a} 2>/dev/null)
-
-            # If the command failed and arch is x86_64, the z-stream doesn't exist and we can stop the loop.
-            # If the command failed and arch is not x86_64, we skip the image lookup.
-            if [ $? != 0 ]; then
-        	echo " Not found."
-        	if [ "${a}" == "x86_64" ]; then
-        	    break 2
-        	else
-        	    continue
-        	fi
-            fi
-            echo " Found."
+            echo -n "Get the release image for OCP ${z}-${a}."
+            IMG=$(oc adm release info --image-for=machine-os-content quay.io/openshift-release-dev/ocp-release:${z}-${a} 2>/dev/null)
 
     	    # Get the image info in JSON format, so we can use jq on it
             IMG_INFO=$(oc image info -o json -a ${PULL_SECRET_FILE} ${IMG} 2>/dev/null)
 
             # If the command failed, we skip the kernel lookup.
-            [ $? != 0 ] && echo "Image info for OCP 4.${y}.${z}-${a} not available" && continue
+            [ $? != 0 ] && echo "Image info for OCP ${z}-${a} not available" && continue
 
             # Add the kernel version from the image labels to the list of kernels
-            KVER=( $(echo ${IMG_INFO} | jq -r ".config.config.Labels[\"com.coreos.rpm.kernel\"]") )
+            KVER=$(echo ${IMG_INFO} | jq -r ".config.config.Labels[\"com.coreos.rpm.kernel-rt-core\"]" | sed -r "s/^(.*)\.rt.*(el[0-9]+.*).${a}/\1.\2/")
+            if [[ ! ${KVERS_OCPVERS_MAP[${KVER}]} ]]; then
+                KVERS_OCPVERS_MAP[${KVER}]=${z}
+            fi
             KVERS+=( ${KVER} )
-            echo "Kernel version for OCP 4.${y}.${z}-${a} is ${KVER}."
+            echo " Kernel version for OCP ${z}-${a} is ${KVER}.${a}."
         done
     done
 done
@@ -47,21 +40,19 @@ done
 # Remove duplicates from the list of kernels and sort it
 IFS=" " read -r -a KVERS <<< "$(tr ' ' '\n' <<< "${KVERS[@]}" | sort -u | tr '\n' ' ')"
 
-# Generate a list of unique kernels without arch
-IFS=" " read -r -a KVERS_NOARCH <<< "$(tr ' ' '\n' <<< "${KVERS[@]}" | sed "s/\..[^.]*$//" | sort -u | tr '\n' ' ')"
-
 # Initialize the matrix file
-echo -n "{ \"versions\": [" > ${MATRIX_FILE}
+echo "{" > ${MATRIX_FILE}
+echo "    \"versions\": [" >> ${MATRIX_FILE}
 
 # Build the matrix from the list of kernel versions
-LAST_KVER_NOARCH=""
+LAST_KVER=""
 COUNT=0
-for KVER_NOARCH in ${KVERS_NOARCH[@]}; do
+for KVER in ${KVERS[@]}; do
     # Extract RHEL version from the kernel version
-    RHEL_VERSION=$(echo ${KVER_NOARCH} | rev | cut -d "." -f 1 | rev | sed -e "s/^el//" -e "s/_/./")
+    RHEL_VERSION=$(echo ${KVER} | rev | cut -d "." -f 1 | rev | sed -e "s/^el//" -e "s/_/./")
 
     # Retrieve UBI image digest for RHEL version
-    UBI_DIGEST=$(oc image info -o json --filter-by-os "linux/amd64" registry.access.redhat.com/ubi8/ubi:${RHEL_VERSION} | jq .digest)
+    UBI_DIGEST=$(oc image info -o json --filter-by-os "linux/amd64" registry.access.redhat.com/ubi8/ubi-minimal:${RHEL_VERSION} 2>/dev/null | jq -r '.digest')
 
     # Initialize the arch with "x86_64" which is mandatory
     ARCH="linux/amd64"
@@ -71,9 +62,9 @@ for KVER_NOARCH in ${KVERS_NOARCH[@]}; do
     for HL_VER in ${HABANA_VERSIONS[@]}; do
         # Check if a habana-ai-driver image exists for this driver and kernel versions
         BUILD_NEEDED="true"
-        DRV_IMG=$(oc image info -a ${PULL_SECRET_FILE} -o json --filter-by-os "linux/amd64" ghcr.io/fabiendupont/habana-ai-driver:${HL_VER}-${KVER_NOARCH}.${ARCH_TAG} 2>/dev/null)
+        DRV_IMG=$(oc image info -a ${PULL_SECRET_FILE} -o json --filter-by-os "linux/amd64" ghcr.io/fabiendupont/habana-ai-driver:${HL_VER}-${KVER}.${ARCH_TAG} 2>/dev/null)
         if [ $? == 0 ]; then
-            echo "Habana AI Driver image for ${HL_VER}-${KVER_NOARCH}.${ARCH_TAG} exists. Checking if base image has changed."
+            echo "Habana AI Driver image for ${HL_VER}-${KVER}.${ARCH_TAG} exists. Checking if base image has changed."
             OLD_UBI_DIGEST=$(echo "${DRV_IMG}" | jq -r ".config.config.Labels[\"org.opencontainers.image.base.digest\"]")
             if [ "${OLD_UBI_DIGEST}" == "${CUR_UBI_DIGEST}" ]; then
                 echo "The UBI ${RHEL_VERSION} has not changed. No need to build."
@@ -82,11 +73,22 @@ for KVER_NOARCH in ${KVERS_NOARCH[@]}; do
         fi
 
         if [ "${BUILD_NEEDED}" == "true" ]; then
+            # Get openshift driver toolkit for the kernel version
+            DRIVER_TOOLKIT_IMAGE=$(oc adm release info ${KVERS_OCPVERS_MAP[${KVER}]} --image-for=driver-toolkit 2>/dev/null)
+
             # Add a comma for all entries but the first one
-            [ ${COUNT} -gt 0 ] && echo -n "," >> ${MATRIX_FILE}
+            [ ${COUNT} -gt 0 ] && echo "," >> ${MATRIX_FILE}
 
             # Add a line for kernel x driver
-            echo -n " { \"rhel\": \"${RHEL_VERSION}\", \"ubi-digest\": ${UBI_DIGEST}, \"kernel\": \"${KVER_NOARCH}\", \"driver\": \"${HL_VER}\", \"arch\": \"${ARCH}\", \"arch_tag\": \"${ARCH_TAG}\" }" >> ${MATRIX_FILE}
+            echo "        {" >> ${MATRIX_FILE}
+            echo "            \"rhel\": \"${RHEL_VERSION}\"," >> ${MATRIX_FILE}
+            echo "            \"ubi-digest\": \"${UBI_DIGEST}\"," >> ${MATRIX_FILE}
+            echo "            \"kernel\": \"${KVER}\"," >> ${MATRIX_FILE}
+            echo "            \"driver\": \"${HL_VER}\"," >> ${MATRIX_FILE}
+            echo "            \"arch\": \"${ARCH}\"," >> ${MATRIX_FILE}
+            echo "            \"arch_tag\": \"${ARCH_TAG}\"," >> ${MATRIX_FILE}
+            echo "            \"driver-toolkit-image\": \"${DRIVER_TOOLKIT_IMAGE}\"" >> ${MATRIX_FILE}
+            echo -n "        }" >> ${MATRIX_FILE}
 
             # Increment counter
             ((COUNT++))
@@ -95,4 +97,6 @@ for KVER_NOARCH in ${KVERS_NOARCH[@]}; do
 done
 
 # Finalize the matrix file
-echo -n " ] }" >> ${MATRIX_FILE}
+echo >> ${MATRIX_FILE}
+echo "    ]" >> ${MATRIX_FILE}
+echo -n "}" >> ${MATRIX_FILE}
